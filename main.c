876493c5e73f03ab0d0cc4733e7b9084c7f4e42b
JenqHorng, PAN <47174263+jenqhorng@users.noreply.github.com>
2019-06-13 12:47:24 +0800

    Creating main.c
    printing 'Hello, World!" on to screen and asking for input 2 numbers for calcuation.
    Charlie

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+  int intger1 = 0;
+  int integer2 = 0;
+  sum = 0;
+  
+  puts("Hello, World!");
+  puts("Please enter two numbers then we will add it for you!");
+  
+  printf("Enter the first integer \n");
+  scanf("%d", &integer1);
+  
+  printf("Enter the second integer \n");
+  scanf("%d", &integer2);
+  
+  sum = integer1 + integer1;
+  
+  printf("Sum of %d + %d = %d", integer1, integer2, sum);
+}
